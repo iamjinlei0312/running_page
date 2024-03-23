15f9c68495aa11803a42d85a1488b014cccd246a
jinlei.run <jinlei0312@gmail.com>
2024-03-23 20:50:51 +0800

    Handling v2.0 conflicts about src/static/site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,17 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://jinlei.run',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'Douyin',
+      url: 'https://www.douyin.com/user/self?modal_id=7334546040516840730&showTab=post',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/iamjinlei0312/running_page/blob/master/README.md',
     },
   ],
 };
