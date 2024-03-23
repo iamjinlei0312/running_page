d64147759afe3646560633a716bb2a5d971c6a25
jinlei.run <jinlei0312@gmail.com>
2024-03-23 20:13:46 +0800

    Handling v2.0 conflicts about src/components/LocationStat/index.tsx

--- src/components/LocationStat/index.tsx
+++ src/components/LocationStat/index.tsx
@@ -2,19 +2,14 @@ import YearStat from '@/components/YearStat';
 import {
   CHINESE_LOCATION_INFO_MESSAGE_FIRST,
   CHINESE_LOCATION_INFO_MESSAGE_SECOND,
-<<<<<<< HEAD:src/components/LocationStat/index.jsx
-} from 'src/utils/const';
-=======
+
 } from '@/utils/const';
->>>>>>> upstream/master:src/components/LocationStat/index.tsx
+
 import CitiesStat from './CitiesStat';
 import LocationSummary from './LocationSummary';
 import PeriodStat from './PeriodStat';
 
-<<<<<<< HEAD:src/components/LocationStat/index.jsx
-const LocationStat = ({ changeYear, changeCity, changeTitle }) => (
-  <div className="w-100 w-100-l pb5 pr5-l">
-=======
+
 interface ILocationStatProps {
   changeYear: (_year: string) => void;
   changeCity: (_city: string) => void;
@@ -23,7 +18,6 @@ interface ILocationStatProps {
 
 const LocationStat = ({ changeYear, changeCity, changeTitle }: ILocationStatProps) => (
   <div className="fl w-100 w-100-l pb5 pr5-l">
->>>>>>> upstream/master:src/components/LocationStat/index.tsx
     <section className="pb4" style={{ paddingBottom: '0rem' }}>
       <p style={{ lineHeight: 1.8 }}>
         {CHINESE_LOCATION_INFO_MESSAGE_FIRST}
